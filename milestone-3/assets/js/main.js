abb713f0248acddcb18362ed000946a98b9d149f
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-27 14:18:11 +0200

    Completo milestone-3

---
+++ milestone-3/assets/js/main.js
@@ -0,0 +1,132 @@
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas'
+    }
+];
+
+
+generateIcons(icons)
+
+
+
+document.getElementById("icons").addEventListener("change", function () {
+    document.querySelector(".cards").innerHTML = ""
+    const selectedIcons = icons.filter(icon => icon.type === this.value)
+    if (this.value == "all") {
+        generateIcons(icons)
+    }
+    generateIcons(selectedIcons)
+})
+
+
+function generateIcons(icons) {
+    icons.forEach(icon => {
+
+        const card =
+            `
+        <div class="col-2">
+            <div class="my_card text-center">
+                <i class="${icon.family} ${icon.prefix}${icon.name} ${icon.type} fa-2x"></i>
+                <div class="icon_name">
+                    <div>${icon.name.toUpperCase()}</div>
+                </div>
+            </div>
+        </div>`
+        document.querySelector('.row').insertAdjacentHTML('beforeend', card)
+    });
+
+}
+
